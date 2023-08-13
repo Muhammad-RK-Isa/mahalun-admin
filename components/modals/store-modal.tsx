09527c92e549e76c92e2645932004e1eefea6e1d
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2023-08-13 16:43:38 +0600

    updated form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,7 +6,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import useStoreModal from "@/hooks/use-store-modal";
 import Modal from "@/components/ui/modal";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const storeFormSchema = z.object({
     name: z.string().min(1, "Store name is required"),
@@ -34,12 +36,38 @@ const StoreModal = () => {
             isOpen={isOpen}
             onClose={onClose}
         >
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Root and Raiment" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
 
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-
-                </form>
-            </Form>
+                            <div className="w-full flex justify-end items-center pt-6 space-x-2">
+                                <Button
+                                    variant="outline"
+                                    onClick 
+                                >
+                                    Cancel
+                                </Button>
+                                <Button>
+                                    Continue
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     )
 }
